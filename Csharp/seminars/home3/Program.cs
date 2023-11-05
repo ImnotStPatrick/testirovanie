e16f71c0432ec5ed3361b3389b51450b4b8d843e
Mary <mary@gmail.com>
2023-11-05 15:02:49 +0300

    Дом3

---
+++ Csharp/seminars/home3/Program.cs
@@ -0,0 +1,57 @@
+﻿// проверка пятизначного палиндрома:
+
+/Console.WriteLine("Введите пятизначное число: ");
+string number = Console.ReadLine();
+int num = number.Length;
+if (num == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.WriteLine("введеное число является палиндромом");
+    }
+    else 
+    {
+        Console.WriteLine("введеное число не палиндром");
+    }
+}
+else 
+{
+    Console.Write("вы ввели не пятизначное число");
+
+}
+
+*/
+
+
+// программа принимает координаты трех точек на вход и находит расстояние между ними в 3д пространстве;
+/ A (3,6,8), B(2,1,-7) -> 15,84 
+int ax = 3;
+int ay = 6;
+int az = 8;
+int bx = 2;
+int by = 1;
+int bz = -7; 
+
+double a = Math.Pow(ax - bx, 2);
+double b = Math.Pow(ay - by, 2);
+double c = Math.Pow(az - bz, 2);
+
+double result = Math.Sqrt(a+b+c);
+Console.WriteLine(result);
+
+
+// программа принимает на вход число N и на выходе выдает таблицу кубов от 1 до N:
+Console.WriteLine("введите число: ");
+string input= Console.ReadLine();
+int number = Convert.ToInt32(input);
+int a = 1;
+Console.Write($"{number} ->  ");
+while (a <= number) 
+{
+    int result = a*a*a; 
+    Console.Write($"{result}, ");
+    a++;
+}
+*/
+
+
