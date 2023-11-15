021a3417a9cc25b5b9f17f62bf5d67ed7fd48979
Mary <mary@gmail.com>
2023-11-15 18:46:03 +0300

    двумерные массивы

---
+++ Csharp/lections/lec4/Program.cs
@@ -0,0 +1,163 @@
+﻿/*string[,] table = new string [2, 5];
+table[1,2]= "слово";
+
+for(int rows = 0; rows < 2; rows++)
+{
+    for(int columns = 0; columns <5; columns++)
+    {
+        Console.WriteLine($"-{table [rows, columns]}-");
+
+    }
+}
+
+
+
+
+
+
+
+void PrintArray(int[,] matr)
+{ 
+    for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+        Console.Write($"{matr[i, j]}  ");
+    }
+    Console.WriteLine();
+}
+}
+
+void FillArray (int[,] matr)
+{ 
+    for (int i = 0; i < matr.GetLength(0); i++)
+{
+    for (int j = 0; j < matr.GetLength(1); j++)
+    {
+       matr[i, j] = new Random().Next(1, 10);
+    }
+
+}
+}
+
+int[,] matrix = new int[3, 4];
+PrintArray(matrix);
+Console.WriteLine();
+FillArray(matrix);
+PrintArray(matrix);
+
+
+
+
+//закрашивание картинки 
+  int[,] pic = new int[,]
+{
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0 },
+{0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0 },
+{0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0 },
+{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },
+};
+
+void PrintImage (int[,] image)
+{
+    for (int i =0; i < image.GetLength(0); i++ )
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            if (image[i, j]==0) Console.Write($" ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 9;
+        FillImage(row - 1, col); // поднимаемя наверх на одну строчку в этом же столбце
+        FillImage(row, col - 1); // Двигаемся влево в этой же строке
+        FillImage(row + 1, col); //спускаемся на строку вниз в этом же столбце
+        FillImage(row, col + 1); //двигаемся вправо в этой же строке
+    }
+}
+PrintImage(pic);
+FillImage(13, 13);
+PrintImage(pic);
+
+
+
+
+//факториал, рекурсия
+double Factorial(int n)
+{
+    if (n==1) return 1;
+    else return n * Factorial(n-1);
+}
+for (int i =1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+// Console.WriteLine(Factorial(8));
+
+
+// числа Фибоначчи
+
+
+double Fibonacci(int n)
+{
+    if (n == 1 || n == 2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 0; i < 40; i++)
+{
+    Console.WriteLine(Fibonacci(i));
+}
+// мой код не работает
+
+
+double Factorial(int n)
+{
+ // 1! = 1
+ // 0! = 1
+ if(n == 1) return 1;
+ else return n * Factorial(n-1);
+}
+for (int i = 1; i < 40; i++)
+{
+ Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+//работает
+
+double Fibonacci(int n)
+{
+ if(n == 1 || n == 2) return 1;
+ else return Fibonacci(n-1) + Fibonacci(n-2);
+}
+for (int i = 1; i < 50; i++)
+{
+ Console.WriteLine($"f({i}) = {Fibonacci(i)}");
+}
+ тоже работает */
+
+ 
